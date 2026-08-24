18aeb803e1016adca63cce7ab71bf3f26a3449d6
anjeetsingh7155 <anjeetsingh7155>
2026-08-24 18:04:47 +0530

    Resolved : Resolveed the Db error

--- src/db/db.ts
+++ src/db/db.ts
@@ -1,4 +1,4 @@
-import mongoose, { Schema, Types } from "mongoose";
+import mongoose, { Schema, Model, Types, model } from "mongoose";
 import { expenseCategories } from "../types";
 import dotenv from "dotenv";
 import dns from "dns";
@@ -11,31 +11,27 @@ try {
 } catch (e) {}
 
 const { databaseURL } = process.env;
+
 const objectId = Types.ObjectId;
 
-const userSchema = new Schema(
-  {
-    id: objectId,
-    email: { type: String, unique: true, required: true },
-    name: { type: String, required: true },
-    password: { type: String, required: true },
-  },
-  { timestamps: true }
-);
+const userSchema = new Schema({
+  id: objectId,
+  email: { type: String, unique: true, required: true },
+  name: { type: String, required: true },
+  password: { type: String, required: true },
+});
 
-const expenseSchema = new Schema(
-  {
-    id: objectId,
-    userId: { type: Types.ObjectId, ref: "users", required: true },
-    title: { type: String, required: true },
-    amount: { type: Number, required: true },
-    category: { type: String, enum: expenseCategories, required: true },
-    date: { type: Date, default: Date.now },
-    notes: { type: String, default: "" },
-  },
-  { timestamps: true }
-);
+const expenseSchema = new Schema({
+  id: objectId,
+  userId: { type: Types.ObjectId, ref: "users", required: true },
+  title: { type: String, required: true },
+  amount: { type: Number, required: true },
+  category: { type: String, enum: expenseCategories, required: true },
+  date: { type: Date, default: Date.now },
+  notes: { type: String, default: "" },
+});
 
+//exported models
 export const userModel = mongoose.model("users", userSchema);
 export const expenseModel = mongoose.model("expenses", expenseSchema);
 
--- src/index.ts
+++ src/index.ts
@@ -1,18 +1,25 @@
 import express from "express";
 import cors from "cors";
 import dotenv from "dotenv";
-import { databaseConnection } from "./db/db";
 import authRoutes from "./routes/authRoutes";
 import expenseRoutes from "./routes/expenseRoutes";
 import profileRoutes from "./routes/profileRoutes";
+import mongoose from "mongoose";
 
 dotenv.config();
 
 const port = process.env.PORT || 5000;
 const app = express();
 
+const databaseURL = process.env.databaseURL
+
+const databaseConnection = () => {
+  return new Promise((resolve, reject) => {
+    mongoose.connect(databaseURL as string).then(resolve).catch(reject);
+  });
+};
 databaseConnection()
-  .then(() => {
+  .then((e) => {
     console.log("dataBase is Connected");
   })
   .catch((e) => {
